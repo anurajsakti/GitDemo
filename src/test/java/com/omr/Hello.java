74f3ce86acaa5f35f5da61c0c47936ea812829d5
rajev <rajevinoth2016@gmail.com>
2026-02-10 12:52:31 +0530

    American guy commit

--- src/test/java/com/omr/Hello.java
+++ src/test/java/com/omr/Hello.java
@@ -3,7 +3,7 @@ package com.omr;
 public class Hello {
     public void t1() {
 		
-		
+		System.out.println("I love my husband");
 	}
 	public void t2() {
 		System.out.println("I love canada");
@@ -11,6 +11,12 @@ public class Hello {
 	}
 	public void t3() {
 		System.out.println("I love my india");
+	}
+	public static void main(String[] args) {
+		Hello h = new Hello();
+		h.t1();
+		h.t2();
+		h.t3();
 	}
 }
 
---
+++ src/test/java/com/omr/Sample.java
@@ -0,0 +1,10 @@
+package com.omr;
+
+public class Sample {
+	
+	
+public static void main(String[] args) {
+	String x = null;
+	System.out.println(x.charAt(1));
+}
+}
