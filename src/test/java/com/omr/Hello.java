8ab2f0b55885c6850012e9eb3b7ef6723e3536d3
rajev <rajevinoth2016@gmail.com>
2026-02-10 12:32:15 +0530

    this is the commit form asian guy

--- src/test/java/com/omr/Hello.java
+++ src/test/java/com/omr/Hello.java
@@ -1,16 +1,16 @@
 package com.omr;
 
 public class Hello {
-public void t1() {
+    public void t1() {
 		
 		
 	}
 	public void t2() {
-		
+		System.out.println("I love canada");
 
 	}
 	public void t3() {
-		
+		System.out.println("I love my india");
 	}
 }
 
