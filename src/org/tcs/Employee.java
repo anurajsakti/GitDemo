e79385d79bec247762d0f1175b5fa00f77c88ad0
rajeswari <anurajsakti22@gmail.com>
2026-06-30 19:23:18 +0530

    first commit

---
+++ src/org/tcs/Employee.java
@@ -0,0 +1,26 @@
+package org.tcs;
+
+public class Employee {
+	public Employee() {
+		this(33243);
+		System.out.println("Default Constructor");
+			}
+	public Employee(int id) {
+		this(74784784874l,"chennai");
+		System.out.println("Int para constructor");
+	}
+	public Employee(String name) {
+		
+		System.out.println("String para constructor");
+	}
+	public Employee(long phno,String name ) {
+		this("Bala");
+		System.out.println("long para constructor");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    Employee e1=new Employee();
+	}
+
+}
