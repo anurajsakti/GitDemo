5f856bb1f613b7d78d5dc14d1caa09355015aad9
rajeswari <anurajsakti22@gmail.com>
2026-07-03 13:17:01 +0530

    Architect

--- src/org/tcs/client.java
+++ src/org/tcs/client.java
@@ -1,8 +1,14 @@
 package org.tcs;
 
 public class client {
-private void sound() {
+	
+public void sound() {
 	// TODO Auto-generated method stub
 System.out.println(" i like sound of water");
+
+}
+public void dog() {
+	// TODO Auto-generated method stub
+System.out.println("I love german Shepherd");
 }
 }
